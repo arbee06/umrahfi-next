1e214aab807d7574f2313afa0ee19a8acbd5b945
Arbi Khalid Hasan <arbikhalidhasan@Arbis-MacBook-Air.local>
2025-06-27 18:26:08 +0300

    6/27/2025

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
     JWT_SECRET: process.env.JWT_SECRET,
